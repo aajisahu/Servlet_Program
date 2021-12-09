e216d1607dc74b6fdfd6669f3013e5739fbf9787
Ashish <89019572+aajisahu@users.noreply.github.com>
2021-12-09 22:04:03 +0530

    Added: Servlet to accept a valid Password

--- src/main/java/com/firstservlet/LoginServlet.java
+++ src/main/java/com/firstservlet/LoginServlet.java
@@ -14,8 +14,8 @@ import java.io.PrintWriter;
         description = "Login Servlet Testing",
         urlPatterns = { "/LoginServlet" },
         initParams = {
-                @WebInitParam(name = "user", value = "Ash"),
-                @WebInitParam(name = "password", value = "BridgeLabz")
+                @WebInitParam(name = "user", value = "Ashish"),
+                @WebInitParam(name = "password", value = "Ashishsahu@1")
         }
 )
 
@@ -33,8 +33,12 @@ public class LoginServlet extends HttpServlet{
         String password = getServletConfig().getInitParameter("password");
 
         // UC3 : Validating name of the user
-        String nameValidate = "^[A-Z][a-z]{2}";
-        if(userID.equals(user) && userID.matches(nameValidate) && password.equals(pwd)) {
+        String nameValidate = "^[A-Z][a-z]{5}";
+
+        // UC4 : Validating password of the user
+        String passwordValidate = "^(?=.*[0-9])(?=[^@#$%^&+=]*[@#$%^&+=][^@#$%^&+=]*$)(?=.*[a-z])(?=.*[A-Z]).{8,}$";
+
+        if(userID.equals(user) && userID.matches(nameValidate) && password.equals(pwd) && password.matches(passwordValidate)) {
             req.setAttribute("user",user);
             req.getRequestDispatcher("LoginSuccess.jsp").forward(req, resp);
         } else {
@@ -45,5 +49,4 @@ public class LoginServlet extends HttpServlet{
         }
 
     }
-
 }
